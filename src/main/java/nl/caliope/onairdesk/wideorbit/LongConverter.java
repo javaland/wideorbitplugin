eea2e8eb820e3b60c3b246919443444e7ff94b78
Nils Dijk <ndijk@caliope.nl>
2013-01-11 13:33:15 +0100

    Created a model for the nowplaying xml;

---
+++ src/main/java/nl/caliope/onairdesk/wideorbit/LongConverter.java
@@ -0,0 +1,37 @@
+package nl.caliope.onairdesk.wideorbit;
+
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+public class LongConverter implements Converter
+{
+
+	@Override
+	@SuppressWarnings("rawtypes")
+	public boolean canConvert(Class type)
+	{
+		return Long.class.equals(type) || long.class.equals(type);
+	}
+
+	@Override
+	public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context)
+	{
+		if (source == null)
+			return;
+		writer.setValue(source.toString());
+	}
+
+	@Override
+	public Long unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context)
+	{
+		try {
+			return Long.decode(reader.getValue());
+		} catch (NumberFormatException ball) {
+			return 0L;
+		}
+	}
+
+}
